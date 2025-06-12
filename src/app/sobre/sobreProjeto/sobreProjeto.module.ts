d61c03abfc493602e22638fa3143b2fcdccd84e2
Lucas Cavalcanti <93608030+Luhsti@users.noreply.github.com>
2025-06-11 21:46:54 -0300

    mudanca de fonte

--- src/app/sobre/sobreProjeto/sobreProjeto.module.ts
+++ src/app/sobre/sobreProjeto/sobreProjeto.module.ts
@@ -1,3 +1,4 @@
+import { AnimateOnVisibleDirective } from './animate-on-visible.directive';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SobreProjetoComponent } from './sobreProjeto.component';
@@ -5,7 +6,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { MaterialModule } from 'src/app/angular-material/material.module';
 import { SobreProjetoRoutingModule } from './sobreProjeto-routing.module';
-import { AnimateOnVisibleDirective } from './animate-on-visible.directive';
+
 
 
 @NgModule({
